163cd731d3977b58966383989c041a5a38987b1b
Kashyap5551 <kash5551@my.yorku.ca>
2022-01-11 16:26:26 -0500

    added the backend

---
+++ server/src/main/java/EMS/project/springboot/EmsBackendApplication.java
@@ -0,0 +1,13 @@
+package EMS.project.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackendApplication.class, args);
+	}
+
+}
